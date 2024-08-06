933ed8c219345f36bb918cef258e7124168c6a4f
wjddnjs1010 <wjddnjs1010@hanyang.ac.kr>
2024-08-06 18:24:23 +0900

    Title: 국영수 순이지, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240806/국영수 순이지/korean-english-math-order.py
@@ -0,0 +1,21 @@
+n = int(input())
+
+class student:
+    def __init__(self,name,kor,eng,mat):
+        self.name = name
+        self.kor = kor
+        self.eng = eng
+        self.mat = mat
+
+students = []
+for i in range(n):
+    name, kor, eng, mat = input().split()
+    kor = int(kor)
+    eng = int(eng)
+    mat = int(mat)
+    students.append(student(name,kor,eng,mat))
+
+students.sort(key=lambda student:(-student.kor,-student.eng,-student.mat))
+
+for i in range(n):
+    print(students[i].name,students[i].kor,students[i].eng,students[i].mat)
