30ec4883d37623e27fa520410a9c5b69ad63cd98
wjddnjs1010 <wjddnjs1010@hanyang.ac.kr>
2024-08-09 20:37:29 +0900

    Title: Date to Date, Time: 127ms, Memory: 24MB, Status: Passed - CodeTree

--- 240809/Date to Date/date-to-date.py
+++ 240809/Date to Date/date-to-date.py
@@ -2,12 +2,12 @@ m1,d1,m2,d2 = tuple(map(int,input().split()))
 m = [31,28,31,30,31,30,31,31,30,31,30,31]
 first = 0
 last = 0
-for i in range(m1):
+for i in range(m1-1):
     first +=m[i]
 first +=d1
 
-for i in range(m2):
+for i in range(m2-1):
     last +=m[i]
 last +=d2
 
-print(last-first if last!=first else 1)
+print(last-first+1)
