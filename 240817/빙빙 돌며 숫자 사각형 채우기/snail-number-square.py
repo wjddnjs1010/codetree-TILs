87e4df15591e3985dc65616090c6a69dff9bcca3
wjddnjs1010 <wjddnjs1010@hanyang.ac.kr>
2024-08-17 22:10:30 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240817/빙빙 돌며 숫자 사각형 채우기/snail-number-square.py
+++ 240817/빙빙 돌며 숫자 사각형 채우기/snail-number-square.py
@@ -1,28 +1,30 @@
-n, m = tuple(map(int, input().split()))
+n,m = tuple(map(int,input().split()))
 answer = [
     [0] * m
-    for _ in range(n)
+    for _ in range()
 ]
 answer[0][0] = 1
 
 def in_range(x, y):
-    return 0 <= x < n and 0 <= y < m
+    return 0 <= x and x < n and 0 <= y and y < m
 
-dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
+dxs,dys = [0,1,0,-1],[1,0,-1,0]
 dr = 0
-x, y = 0, 0
-for i in range(2, n * m + 1):
-    x_nxt, y_nxt = x + dxs[dr], y + dys[dr]
-    if in_range(x_nxt, y_nxt) and answer[x_nxt][y_nxt] == 0:
-        answer[x_nxt][y_nxt] = i
+x,y = 0,0
+for i in range(2,n*m+1):
+    x_nxt,y_nxt = x+dxs[dr],y+dys[dr]
+    if in_range(x_nxt,y_nxt) and answer[x_nxt][y_nxt]==0:
+        answer[x_nxt][y_nxt]=i
     else:
-        dr = (dr + 1) % 4
-        x_nxt, y_nxt = x + dxs[dr], y + dys[dr]
-        answer[x_nxt][y_nxt] = i
+        dr =(dr+1)%4
+        x_nxt,y_nxt = x+dxs[dr],y+dys[dr]
+        answer[x_nxt][y_nxt]=i
 
-    x, y = x_nxt, y_nxt
+
+    x,y = x_nxt,y_nxt
+
 
 for i in range(n):
     for j in range(m):
-        print(answer[i][j], end=' ')
+        print(answer[i][j], end = ' ')
     print()
