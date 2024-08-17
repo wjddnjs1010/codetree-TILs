1c9ad9be7004b61a06edd9152816b95eebd01c17
wjddnjs1010 <wjddnjs1010@hanyang.ac.kr>
2024-08-17 11:11:01 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240817/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 240817/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -1,71 +1,38 @@
-N, K, P, T = tuple(map(int,input().split()))
+N, K, P, T = map(int, input().split())
 
-sh = [0]*N
-gam = [0]*N
-gam[P-1] = 1
+sh = [0] * N
+gam = [0] * N
+gam[P-1] = 1  # Assuming P is within [1, N]
 shs = []
 
 class dev:
-    def __init__(self,t,x,y):
+    def __init__(self, t, x, y):
         self.t = t
         self.x = x
         self.y = y
 
 for i in range(T):
-    t,x,y = tuple(map(int,input().split()))
-    shs.append(dev(t,x,y))
-shs.sort(key = lambda dev:dev.t)
+    t, x, y = map(int, input().split())
+    if 1 <= x <= N and 1 <= y <= N:
+        shs.append(dev(t, x, y))
+
+shs.sort(key=lambda dev: dev.t)
 
 for j in range(T):
-    if gam[shs[j].x-1] > 0:
-        sh[shs[j].x-1]+=1
-        if sh[shs[j].x-1]<=K:
-            gam[shs[j].y-1]+=1
+    if 1 <= shs[j].x <= N and 1 <= shs[j].y <= N:
+        if gam[shs[j].x-1] > 0:
+            sh[shs[j].x-1] += 1
+            if sh[shs[j].x-1] <= K:
+                gam[shs[j].y-1] += 1
 
-    if gam[shs[j].y-1]>0:
-        sh[shs[j].y-1]+=1
-        if sh[shs[j].y-1]<=K:
-            gam[shs[j].x-1]+=1
+        if gam[shs[j].y-1] > 0:
+            sh[shs[j].y-1] += 1
+            if sh[shs[j].y-1] <= K:
+                gam[shs[j].x-1] += 1
 
-
-j=0
-shs[0].x-1
-out = [0]*N
+out = [0] * N
 for i in range(N):
-    if gam[i]>0:
-        out[i]=1
+    if gam[i] > 0:
+        out[i] = 1
 
-print(''.join(map(str,out)))
-
-'''
-P = 2
-gam = 0100
-x = 2 y = 3
-if gam[devs[j].(x-1)]>1:
-    sh[devs[j].(x-1)]+=1
-    if sh[devs[j].(x-1)]<K:
-        gam[devs[j].(y-1)]+=1
-
-if gam[devs[j].(y-1)]>1:
-    sh[devs[j].(y-1)]+=1
-    if sh[devs[j].(y-1)]<K:
-        gam[devs[j].(x-1)]+=1
-sh = 0100
-gam = 0110
-x = 2 y = 4
-sh = 0200
-gam = 0111
-x = 1 y = 2
-sh = 0300
-gam = 0111
-
-gam,sh
-0100
-
-0110
-0100
-0111
-0200
-0111
-03
-'''
+print(''.join(map(str, out)))
