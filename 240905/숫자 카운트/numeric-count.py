f0abad23e7e1d8c6f4eb2852fc0219d122d51315
wjddnjs1010 <wjddnjs1010@hanyang.ac.kr>
2024-09-05 18:39:48 +0900

    Title: 숫자 카운트, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240905/숫자 카운트/numeric-count.py
+++ 240905/숫자 카운트/numeric-count.py
@@ -9,11 +9,8 @@ def count_matches(a, b):
     for i in range(3):
         if a[i] == b[i]:
             count1 += 1
-
-    for i in range(3):
-        if b[i] in a and a[i]!=b[i]:
+        elif b[i] in a:
             count2 += 1
-            break
     
     return count1, count2
 
@@ -46,7 +43,7 @@ for i in range(123, 988):  # 123부터 987까지 가능한 세 자리 수
         # 주어진 카운트 정보와 실제 카운트가 일치하지 않으면 후보에서 제외
         if actual_count1 != expected_count1 or actual_count2 != expected_count2:
             is_possible = False
-            False
+            break
     
     if is_possible:
         possible_count += 1
