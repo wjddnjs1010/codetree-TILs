f8f59eda5b9709b268bd1d3dc9a16740b345d446
wjddnjs1010 <wjddnjs1010@hanyang.ac.kr>
2024-09-06 21:16:50 +0900

    Title: 이상한 폭탄 2, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240906/이상한 폭탄 2/strange-bomb-2.py
@@ -0,0 +1,11 @@
+n,k = map(int,input().split())
+
+arr = [int(input()) for _ in range(n)]
+max_num = 0
+for i in range(n):
+    for j in range(i+1,n):
+        if arr[i] ==arr[j] and j-i<=k:
+            num = arr[i]
+            max_num = max(max_num,num)
+
+print(max_num)
