d85a554a75db63b9751dfb940c853d8caaf4a4b8
wjddnjs1010 <wjddnjs1010@hanyang.ac.kr>
2024-09-06 21:17:52 +0900

    Title: 이상한 폭탄 2, Time: 114ms, Memory: 25MB, Status: Passed - CodeTree

--- 240906/이상한 폭탄 2/strange-bomb-2.py
+++ 240906/이상한 폭탄 2/strange-bomb-2.py
@@ -1,7 +1,7 @@
 n,k = map(int,input().split())
 
 arr = [int(input()) for _ in range(n)]
-max_num = 0
+max_num = -1
 for i in range(n):
     for j in range(i+1,n):
         if arr[i] ==arr[j] and j-i<=k:
