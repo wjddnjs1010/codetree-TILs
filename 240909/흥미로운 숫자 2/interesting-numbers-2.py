14f79e9f79d49b053ac7c5feb9dba6f2a10ac8e8
wjddnjs1010 <wjddnjs1010@hanyang.ac.kr>
2024-09-09 11:49:42 +0900

    Title: 흥미로운 숫자 2, Time: 324ms, Memory: 25MB, Status: Passed - CodeTree

--- 240909/흥미로운 숫자 2/interesting-numbers-2.py
+++ 240909/흥미로운 숫자 2/interesting-numbers-2.py
@@ -1,14 +1,17 @@
 x,y = input().split()
 num_int = 0
 for h in range(int(x),int(y)+1):
-    count = 0
-    
+    min_count = float('inf')
     h = str(h)
+
     for i in range(len(h)):
-        for j in range(i+1,len(h)):
-            if h[i]==h[j]:
+        count = 0
+        for j in range(len(h)):
+            if i !=j and h[i]!=h[j]:
                 count+=1
-    if count ==1:
+        min_count = min(min_count, count)
+                #print(min_count)
+    if min_count ==1:
         num_int+=1
 
 print(num_int)
